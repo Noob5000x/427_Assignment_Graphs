238aabe4ecd35c456b3a10eff9fef74ffe27486a
Christella Marie Perez Taguicana <christellamarie.taguicana01@student.csulb.edu>
2025-09-03 20:50:15 -0700

    Updated main.py
    Implemented error handling for reading the input file

--- main.py
+++ main.py
@@ -15,19 +15,31 @@ parser.add_argument("--output")
 
 args = parser.parse_args()
 
+# Generate Graph
 if args.create_random_graph:
     n, c = args.create_random_graph
     p = c * math.log(n) / n if n > 1 else 0
-    ## graph = nx.erdos_renyi_graph(n, p)
-    ## mapping = {i: str(i) for i in range(n)}
-    ## graph = nx.relabel_nodes(graph, mapping)
+    graph = nx.erdos_renyi_graph(n, p)
+    mapping = {i: str(i) for i in range(n)}
+    graph = nx.relabel_nodes(graph, mapping)
+    print(f'Generated a random graph with {n} nodes and p={p:.4f}.')
 elif args.input:
-    graph = nx.read_gml(args.input)
-    print("Nodes: ", graph.nodes())
-    print("Edges: ", graph.edges())
-    pass # reads given graph, is overriden by create_random_graph
+    try:
+        graph = nx.read_gml(args.input)
+        print("Successfully read graph from {args.input}.")
+        print("Nodes: ", graph.nodes())
+        print("Edges: ", graph.edges())
+    # pass # reads given graph, is overriden by create_random_graph
+    except FileNotFoundError:
+        print(f"Error: The file '{args.input}' was not found.")
+        exit(1)
 else:
     print("Missing arguments (--input graph_file.gml or --create_random_graph n c)\n")
     exit(1)
-    
 
+# Multi-source BFS w/ Path Tracking
+if args.multi_BFS:
+    for startingNode in args.multi_BFS:
+        if startingNode not in graph:
+            print(f"Node '{startingNode}' was not found in the graph.")
+            continue
